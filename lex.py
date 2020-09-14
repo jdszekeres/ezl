b4026ff0751e7ad5b34193f88fddcc88a43b6c60
jdszekeres <69050063+jdszekeres@users.noreply.github.com>
2020-09-14 09:15:30 -0700

    allow escape characters

--- lex.py
+++ lex.py
@@ -85,8 +85,6 @@ class Lexer:
             while self.curChar != '\"':
                 # Don't allow special characters in the string. No escape characters, newlines, tabs, or %.
                 # We will be using C's printf on this string.
-                if self.curChar == '\r' or self.curChar == '\n' or self.curChar == '\t' or self.curChar == '\\' or self.curChar == '%':
-                    self.abort("Illegal character in string.")
                 self.nextChar()
 
             tokText = self.source[startPos : self.curPos] # Get the substring.
